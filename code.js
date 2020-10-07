2bc63c0071b0281b1ec2f745e9fec77505fc9b1d
Jacob Goodwin <53794957+DistantJragon@users.noreply.github.com>
2020-10-07 18:47:43 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,2 +1,11 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
+function init() {
+    canvas = document.getElementById("theCanvas");
+    canvas.width = document.body.clientWidth;
+    canvas.height = document.body.clientHeight;
+    canvasW = canvas.width;
+    canvasH = canvas.height;
+
+    if (canvas.getContext) {
+        setInterval(run , 33);
+    }
+}

9d9a5dd071cc1e9f8a794645935d0ac3ff0643d2
Jacob Goodwin <53794957+DistantJragon@users.noreply.github.com>
2020-10-09 14:53:37 -0400

    setup 1

--- code.js
+++ code.js
@@ -2,8 +2,13 @@ var canvas = document.getElementById("theCanvas");
 canvas.width = document.body.clientWidth;
 canvas.height = document.body.clientHeight;
 var ctx = canvas.getContext("2d");
-function init() {
+
+imageList = []
+function startGameLoop() {
     if (canvas.getContext) {
         setInterval(gameLoop(), 10);
     }
+}
+function gameLoop() {
+
 }

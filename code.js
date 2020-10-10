885fc80ac8c38ee30a408e5f460f31b9e8ccdb57
Jacob Goodwin <53794957+DistantJragon@users.noreply.github.com>
2020-10-10 11:31:27 -0400

    fix drawing, more shapes

--- code.js
+++ code.js
@@ -5,9 +5,9 @@ var gameArea = {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     },
     updateSprites: function() {
-        for (list in drawPriorityList) {
+        for (list of drawPriorityList) {
             if (list instanceof Array) {
-                for (sprite in drawPriorityList[i]) {
+                for (sprite of list) {
                     sprite.update();
                 }
             }
@@ -23,6 +23,7 @@ shapeList = []
 textList = []
 
 var camera = {x: 0, y: 0};
+var gradient;
 
 function createNewImage(width, height, source, x, y, drawPriority) {
     this.image = new Image();
@@ -61,11 +62,13 @@ function createNewImage(width, height, source, x, y, drawPriority) {
     }
 }
 
-function createNewShape(width, height, color, shape, x, y, drawPriority) {
+function createNewShape(shape, fillStyle, stokeWidth, strokeStyle, width, height, x, y, drawPriority) {
     this.width = width;
     this.height = height;
-    this.color = color
-    this.shape = shape
+    this.fillStyle = fillStyle;
+    this.stokeWidth = stokeWidth;
+    this.strokeStyle = strokeStyle;
+    this.shape = shape;
     this.acceleration = 0;
     this.velocity = 0;
     this.direction = 0;
@@ -78,15 +81,17 @@ function createNewShape(width, height, color, shape, x, y, drawPriority) {
     shapeList.push(this)
     this.update = function() {
         var ctx = gameArea.ctx;
-        ctx.fillStyle = this.color;
-        if (this.shape = 'rectangle') {
-            ctx.fillRect(
-                this.x - camera.x, this.y - camera.y, 
-                this.width, this.height
-            );
+        ctx.beginPath();
+        if (this.shape == "rectangle") { ctx.rect(x - camera.x, this.y - camera.y, this.width, this,height);
+        } else if (this.shape == "circle") { ctx.arc(this.x, this.y, this.width, 0, 2 * Math.PI);
         }
+        ctx.fillStyle = this.fillStyle;
+        ctx.fill();
+        ctx.lineWidth = this.stokeWidth;
+        ctx.strokeStyle = this.strokeStyle;
+        ctx.stroke();
     }
-    this.move() = function() {
+    this.move = function() {
         this.velocity += this.acceleration;
         this.x += this.velocity * Math.cos(this.direction);
         this.y += this.velocity * Math.sin(this.direction);
@@ -121,15 +126,16 @@ function createNewText(fontSize, font, color, x, y, drawPriority) {
 }
 
 function startGameLoop() {
-    if (canvas.getContext) {
-        setInterval(gameLoop(), 10);
+    if (gameArea.canvas.getContext) {
+        setInterval(gameLoop, 10);
     }
 }
 function gameLoop() {
-    gameArea.clear()
-    gameArea.updateSprites()
+    gameArea.clear();
+    gameArea.updateSprites();
 }
 
 function randomInteger(min, max) {;
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+startGameLoop();
